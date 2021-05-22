da99060b3c8227e2df3b457168c24f1ddd8b63b1
Ankit Muchhala <muchhalaankit@gmail.com>
2021-05-23 01:17:01 +0530

    fix styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react"
 import Header from "./Header"
 import Main from "./Main"
+import GlobalStyle from "./GlobalStyles"
 
 const Layout = ({ active, children, location }) => {
   const rootPath = `${__PATH_PREFIX__}/`
 
   return (
     <Fragment>
+      <GlobalStyle />
       <Header active={active} />
       <Main className={location.pathname !== rootPath && "offset-header"}>
         {children}
