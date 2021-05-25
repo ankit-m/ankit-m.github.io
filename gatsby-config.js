b1b8299600650a28d210f856b0e277930f74712d
Ankit Muchhala <muchhalaankit@gmail.com>
2021-05-25 17:38:19 +0530

    add new logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,7 +120,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/construction.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
